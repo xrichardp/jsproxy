2db6617ed64c21cccab28ded0ccc2792e78047bc
xrichardp <xrichardp@mail.com>
2019-07-09 19:56:27 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -41,10 +41,10 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'a.007.workers.dev': 1,
-        'a.hehe.workers.dev': 1,
-        'a.lulu.workers.dev': 1,
-        'shrill-unit-8594.jsproxy.workers.dev': 1,
+        
+        
+        
+        'twilight-scene-6f38.xrichard.workers.dev': 1,
       }
     }
   },
